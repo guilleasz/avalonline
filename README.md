604f7921399018642e6083fda5f622855623f0b1
Guillermo Aszyn <guilleaszyn@gmail.com>
2017-06-22 10:33:06 -0400

    basic boilerplate

---
+++ README.md
@@ -0,0 +1,8 @@
+# Spacialon
+
+Board game mobile web app 
+
+### Authors:
+- Jacob Han 
+- Yimin Wang
+- Guillermo Aszyn
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+  entry: './browser/index.js',
+  output: {
+    path: __dirname,
+    filename: './server/public/javascript/bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
