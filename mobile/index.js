1b8a721b37b319dd5708d574c58d8d5bc97b3494
Yi Min Wang <yimin@Yis-MacBook-Pro.local>
2017-06-22 16:21:33 -0400

    can add player to firebase database

--- mobile/index.js
+++ mobile/index.js
@@ -4,14 +4,14 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory, Route, IndexRoute } from 'react-router';
 import store from './store';
 import JoinLobbyContainer from './containers/JoinLobbyContainer';
-import JoinLobby from './components/JoinLobby';
+import LobbyContainer from './containers/LobbyContainer';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/mobile">
         <IndexRoute component={JoinLobbyContainer} />
-        <Route path="play/:lobbyId" />
+        <Route path="play/:lobbyId" component={LobbyContainer} />
       </Route>
     </Router>
   </Provider>, document.getElementById('app'));
