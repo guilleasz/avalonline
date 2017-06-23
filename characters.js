c12e892aca1767dbd5bbd97cde5906a3092edd80
Guillermo Aszyn <guilleaszyn@gmail.com>
2017-06-23 10:43:39 -0400

    characters names

---
+++ characters.js
@@ -0,0 +1,7 @@
+export const MERLIN = 'Merlin';
+export const MORDERED = 'Mordered';
+export const MORGANA = 'Morgana';
+export const PERCIVAL = 'Percival';
+export const OBERON = 'Oberon';
+export const ASSASSIN = 'Assassin';
+export const GOOD_GUY = 'Good Guy';
