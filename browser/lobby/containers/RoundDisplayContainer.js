96737bc938dfa56f2d694e55334fd17dbefbb31b
Yi Min Wang <yimin@Yis-MacBook-Pro.local>
2017-06-23 16:19:04 -0400

    shows all rounds and shows how rounds were passed/failed

---
+++ browser/lobby/containers/RoundDisplayContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { firebaseConnect, dataToJS } from 'react-redux-firebase';
+import RoundDisplay from '../components/RoundDisplay';
+
+class RoundDisplayContainer extends React.Component {
+
+  render() {
+    const { roundHistory } = this.props;
+    console.log(roundHistory);
+    return (
+      <div>
+        <h1>Round Pass/Fail</h1>
+        {
+          roundHistory.map((info, i) => (<RoundDisplay key={i} roundNumber={i+1} roundInfo={info} />))
+        }
+      </div>
+    );
+  }
+}
+
+const wrappedRoundDisplayContainer = firebaseConnect(['/'])(RoundDisplayContainer);
+
+export default connect(({ firebase }, ownProps) => ({
+  roundHistory: dataToJS(firebase, `${ownProps.lobbyId}/gameState/roundHistory`),
+}))(wrappedRoundDisplayContainer);
