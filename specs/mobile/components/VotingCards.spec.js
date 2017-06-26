279af45cd06f8d3c5252acf231254c00cb70cb90
Guillermo Aszyn <guilleaszyn@gmail.com>
2017-06-26 11:17:09 -0400

    fixed webpack bug

---
+++ specs/mobile/components/VotingCards.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import VotingCards from '../../../mobile/components/VotingCards';
+
+chai.use(sinonChai);
+
+
+describe('ConfirmButton Component', () => {
+
+});
