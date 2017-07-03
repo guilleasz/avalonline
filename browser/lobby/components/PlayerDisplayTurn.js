fae34ee80784a7bfc1691ac879922aae4109422e
Yi Min Wang <yimin@Yis-MacBook-Pro.fios-router.home>
2017-07-02 20:04:54 -0400

    spacing

--- browser/lobby/components/PlayerDisplayTurn.js
+++ browser/lobby/components/PlayerDisplayTurn.js
@@ -7,36 +7,36 @@ const PlayerDisplayTurn = ({ player, isQuestLeader, onQuest, numPlayers, questAp
   let playerPicture = null;
   let loaded = false;
   return (
-  <div className={`flex-item player-card players_${numPlayers} ${isQuestLeader ? 'quest-leader' : ''} ${onQuest ? 'on-quest' : ''}`}>
-    <div className={`${isQuestLeader ? 'quest-leader' : ''} ${onQuest ? 'on-quest' : ''}`} >
-      <div className="player-picture" ref={(elem) => { playerPicture = elem; }} >
-        {
-          player.downloadURL ? (() => {
-            loadImage(player.downloadURL, (img) => {
-              if (playerPicture && !loaded) {
-                loaded = true;
-                playerPicture.appendChild(img);
-              }
-            }, {
-              maxHeight: 500,
-              maxWidth: 500,
-              orientation: player.orientation ? player.orientation : true,
-            });
-            return null;
-          })()
-          :
-          <img alt="Player" width="100%" src="/assets/card_back_player.png" />
-        }
-      </div>
-      <VoteCard
-        questApprovalVote={questApprovalVote}
-        showCard={showCard}
-      />
-      <div className="player-card-name">
-        <div>{player.name}</div>
+    <div className={`flex-item player-card players_${numPlayers} ${isQuestLeader ? 'quest-leader' : ''} ${onQuest ? 'on-quest' : ''}`}>
+      <div className={`${isQuestLeader ? 'quest-leader' : ''} ${onQuest ? 'on-quest' : ''}`} >
+        <div className="player-picture" ref={(elem) => { playerPicture = elem; }} >
+          {
+            player.downloadURL ? (() => {
+              loadImage(player.downloadURL, (img) => {
+                if (playerPicture && !loaded) {
+                  loaded = true;
+                  playerPicture.appendChild(img);
+                }
+              }, {
+                maxHeight: 500,
+                maxWidth: 500,
+                orientation: player.orientation ? player.orientation : true,
+              });
+              return null;
+            })()
+            :
+            <img alt="Player" width="100%" src="/assets/card_back_player.png" />
+          }
+        </div>
+        <VoteCard
+          questApprovalVote={questApprovalVote}
+          showCard={showCard}
+        />
+        <div className="player-card-name">
+          <div>{player.name}</div>
+        </div>
       </div>
     </div>
-  </div>
-);
-}
+  );
+};
 export default PlayerDisplayTurn;
